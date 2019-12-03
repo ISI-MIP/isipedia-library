6e3c986b9d0eea24ec767e1e1ef58af6cc474398
perrette <mahe.perrette@pik-potsdam.de>
2019-12-03 12:21:06 +0100

    isipedia library

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+import versioneer
+
+setup(name='Isipedia Library',
+      version=versioneer.get_version(),
+      cmdclass = versioneer.get_cmdclass(),
+      author='Mahe Perrette for ISIpedia',
+      author_email='mahe.perrette@pik-potsdam.de',
+      description='utilities to crunch data for ISIpedia',
+      url='git@github.com:ISI-MIP/isipedia-library.git',
+      packages=['isipedia'],
+      scripts=[],
+      license = "MIT",
+      requires = [],
+      )
+
